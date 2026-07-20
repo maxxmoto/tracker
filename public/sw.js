a83b07221da0424a8ec792c48f7ab3a48654d23f
maxxmoto <maxxmoto@users.noreply.github.com>
2026-07-20 23:07:39 +0300

    PWA: manifest, service worker, yellow-black icons

---
+++ public/sw.js
@@ -0,0 +1,34 @@
+const CACHE = "foodcourt-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/icon.svg",
+  "/favicon.svg"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request).then((res) => {
+      const clone = res.clone();
+      if (res.status === 200) {
+        caches.open(CACHE).then((c) => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() => caches.match("/")))
+  );
+});
